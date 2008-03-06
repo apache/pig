3998587f9d2b76966e689098fdbcfc39917b17e1
Olga Natkovich <olga@apache.org>
2008-03-06 23:13:15 +0000

    PIG-94: compile fix
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pig/trunk@634453 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/impl/streaming/PigExecutableManager.java
+++ src/org/apache/pig/impl/streaming/PigExecutableManager.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pig.backend.streaming;
+package org.apache.pig.impl.streaming;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
