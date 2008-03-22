37dec9c4cf044ecf00229c44e1eee989ac27faf9
Olga Natkovich <olga@apache.org>
2008-03-22 02:42:39 +0000

    PIG-154: moved define and store into QueryParser from Grunt parser
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pig/trunk@639939 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/tools/grunt/GruntParser.java
+++ src/org/apache/pig/tools/grunt/GruntParser.java
@@ -112,10 +112,6 @@ public class GruntParser extends PigScriptParser {
         mDone = true;
     }
     
-    protected void processRegisterFunc(String name, String expr) {
-        mPigServer.registerFunction(name, expr);
-    }
-    
     protected void processDescribe(String alias) throws IOException {
         mPigServer.dumpSchema(alias);
     }
@@ -158,10 +154,6 @@ public class GruntParser extends PigScriptParser {
         }
     }
     
-    protected void processStore(String alias, String file, String func) throws IOException {
-        mPigServer.store(alias, file, func);
-    }
-        
     protected void processCat(String path) throws IOException
     {
         try {
