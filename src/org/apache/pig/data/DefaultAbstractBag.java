83b76d7e1d1b4af547f833cee5e520ac77933515
Olga Natkovich <olga@apache.org>
2008-07-16 23:33:43 +0000

    PIG-129 and PIG-164 merge
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pig/branches/types@677466 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/data/DefaultAbstractBag.java
+++ src/org/apache/pig/data/DefaultAbstractBag.java
@@ -31,12 +31,17 @@ import java.util.ArrayList;
 import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.impl.physicalLayer.PhysicalOperator;
 import org.apache.pig.impl.util.Spillable;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Default implementation of DataBag.  This is the an abstract class used as a
  * parent for all three of the types of data bags.
  */
 public abstract class DefaultAbstractBag implements DataBag {
+
+     private static final Log log = LogFactory.getLog(DataBag.class);
+
     // Container that holds the tuples. Actual object instantiated by
     // subclasses.
     protected Collection<Tuple> mContents;
@@ -302,6 +307,32 @@ public abstract class DefaultAbstractBag implements DataBag {
             mSpillFiles = new ArrayList<File>(1);
         }
 
+        String tmpDirName= System.getProperties().getProperty("java.io.tmpdir") ;                
+        File tmpDir = new File(tmpDirName);
+  
+        // if the directory does not exist, create it.
+        if (!tmpDir.exists()){
+            log.info("Temporary directory doesn't exists. Trying to create: " + tmpDir.getAbsolutePath());
+          // Create the directory and see if it was successful
+          if (tmpDir.mkdir()){
+            log.info("Successfully created temporary directory: " + tmpDir.getAbsolutePath());
+          } else {
+              // If execution reaches here, it means that we needed to create the directory but
+              // were not successful in doing so.
+              // 
+              // If this directory is created recently then we can simply 
+              // skip creation. This is to address a rare issue occuring in a cluster despite the
+              // the fact that spill() makes call to getSpillFile() in a synchronized 
+              // block. 
+              if (tmpDir.exists()) {
+                log.info("Temporary directory already exists: " + tmpDir.getAbsolutePath());
+              } else {
+                log.error("Unable to create temporary directory: " + tmpDir.getAbsolutePath());
+                throw new IOException("Unable to create temporary directory: " + tmpDir.getAbsolutePath() );                  
+              }
+          }
+        }
+        
         File f = File.createTempFile("pigbag", null);
         f.deleteOnExit();
         mSpillFiles.add(f);
