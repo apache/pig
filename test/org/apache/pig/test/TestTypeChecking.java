b2c92bc931b9f20157cf8bd1fde0caebb37565c3
Olga Natkovich <olga@apache.org>
2009-05-22 00:29:08 +0000

    PIG-811: Globs with ? in the pattern are broken in local mode (hagleitn via olgan)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/pig/trunk@777334 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/test/TestTypeChecking.java
+++ test/org/apache/pig/test/TestTypeChecking.java
@@ -244,7 +244,7 @@ public class TestTypeChecking extends TestCase {
 
     public void testSUM1() throws Throwable {
         TypeCheckingTestUtil.printCurrentMethodName() ;
-        planTester.buildPlan("a = load ':INPATH:/singlefile/studenttab10k' as (name:chararray, age:int, gpa:double);") ;
+        planTester.buildPlan("a = load '/user/pig/tests/data/singlefile/studenttab10k' as (name:chararray, age:int, gpa:double);") ;
         LogicalPlan plan1 = planTester.buildPlan("b = foreach a generate (long)age as age:long, (int)gpa as gpa:int;") ;
         LogicalPlan plan2 = planTester.buildPlan("c = foreach b generate SUM(age), SUM(gpa);") ;
         planTester.typeCheckPlan(plan2);
