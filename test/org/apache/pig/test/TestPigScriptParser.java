393c36f92112eecdd0ae3825d0c4df36da0a53af
Pradeep Kamath <pradeepkth@apache.org>
2009-09-22 17:33:24 +0000

    PIG-738: Regexp passed from pigscript fails in UDF  (pradeepkth)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/pig/trunk@817739 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/test/TestPigScriptParser.java
+++ test/org/apache/pig/test/TestPigScriptParser.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -32,6 +33,7 @@ import junit.framework.TestCase;
 
 import org.apache.pig.PigServer;
 import org.apache.pig.ExecType;
+import org.apache.pig.data.Tuple;
 import org.apache.pig.impl.PigContext;
 import org.apache.pig.impl.plan.OperatorKey;
 import org.apache.pig.impl.logicalLayer.* ;
@@ -77,6 +79,17 @@ public class TestPigScriptParser extends TestCase {
         }
         
         {
+            // escaping dot
+            // the reason we have 4 backslashes below is we really want to put two backslashes but
+            // since this is to be represented in a Java String, we escape each backslash with one more
+            // backslash - hence 4. In a pig script in a file, this would be
+            // \\.string
+            String query = "B2 = filter A by $0 eq 'This is a test \\\\.string' ;" ;
+            checkParsedConstContent(aliases, opTable, pigContext, aliasOp, fileNameMap,
+                                    query, "This is a test \\.string") ;  
+        }
+        
+        {
         	// newline condition
         	String query = "B3 = filter A by $0 eq 'This is a test \\nstring' ;" ;
         	checkParsedConstContent(aliases, opTable, pigContext, aliasOp, fileNameMap, 
@@ -90,7 +103,38 @@ public class TestPigScriptParser extends TestCase {
         	                        query, "This is a test \uD30C\uC774string") ;	
         }
     }
-
+    
+    @Test
+    public void testDefineUDF() throws Exception {
+        String inputData[] = {
+                "dshfdskfwww.xyz.com/sportsjoadfjdslpdshfdskfwww.xyz.com/sportsjoadfjdsl" ,
+                "kas;dka;sd" ,
+                "jsjsjwww.xyz.com/sports" ,
+                "jsdLSJDcom/sports" ,
+                "wwwJxyzMcom/sports"
+        };
+        File f = Util.createFile(inputData);
+        String[] queryLines = new String[] { 
+                // the reason we have 4 backslashes below is we really want to put two backslashes but
+                // since this is to be represented in a Java String, we escape each backslash with one more
+                // backslash - hence 4. In a pig script in a file, this would be
+                // www\\.xyz\\.com
+                "define minelogs org.apache.pig.test.RegexGroupCount('www\\\\.xyz\\\\.com/sports');" ,
+        		"A = load 'file://" + f.getAbsolutePath() + "'  using PigStorage() as (source : chararray);" ,
+        		"B = foreach A generate minelogs(source) as sportslogs;" };
+        PigServer ps = new PigServer(ExecType.LOCAL);
+        for (String line : queryLines) {
+            ps.registerQuery(line);
+        }
+        Iterator<Tuple> it = ps.openIterator("B");
+        int[] expectedResults = new int[] {2,0,1,0,0};
+        int i = 0;
+        while(it.hasNext()) {
+            Tuple t = it.next();
+            assertEquals(expectedResults[i++], t.get(0));
+        }
+    }
+    
 	private void checkParsedConstContent(Map<LogicalOperator, LogicalPlan> aliases,
                                              Map<OperatorKey, LogicalOperator> opTable,
                                              PigContext pigContext,
