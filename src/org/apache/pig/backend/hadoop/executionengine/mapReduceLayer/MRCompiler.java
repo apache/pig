7db6f1b88e7d7e4d092d88f5a288d126edef2b95
Olga Natkovich <olga@apache.org>
2009-12-09 16:09:09 +0000

    PIG-1134: Skewed Join sampling job overwhelms the name node (sriranjan via olgan)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/pig/trunk@888852 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/MRCompiler.java
+++ src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/MRCompiler.java
@@ -1844,7 +1844,7 @@ public class MRCompiler extends PhyPlanVisitor {
     		String inputFile = lFile.getFileName();
 
     		return getSamplingJob(sort, prevJob, transformPlans, lFile, sampleFile, rp, null, 
-    							PartitionSkewedKeys.class.getName(), new String[]{per, mc, inputFile}, PoissonSampleLoader.class.getName());
+    							PartitionSkewedKeys.class.getName(), new String[]{per, mc, inputFile}, RandomSampleLoader.class.getName());
     	}catch(Exception e) {
     		throw new PlanException(e);
     	}
