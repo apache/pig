7967b3584e88e8c20c63f44aae1f6fcf6a028ba8
Jianyong Dai <daijy@apache.org>
2009-12-11 18:42:29 +0000

    PIG-1142: Got NullPointerException merge join with pruning
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/pig/branches/branch-0.6@889758 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/impl/logicalLayer/LOFilter.java
+++ src/org/apache/pig/impl/logicalLayer/LOFilter.java
@@ -268,7 +268,8 @@ public class LOFilter extends RelationalOperator {
                     .warn("Cannot prune columns in filter, no schema information found");
             return false;
         }
-        for (Pair<Integer, Integer> column : columns) {
+        for (int i=columns.size()-1;i>=0;i--) {
+            Pair<Integer, Integer> column = columns.get(i);
             if (column.first != 0) {
                 int errCode = 2191;
                 throw new FrontendException(
