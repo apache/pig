6e74ce71d22b5ec4547e72ee914ab41fe5ead29b
Jianyong Dai <daijy@apache.org>
2009-12-25 00:16:08 +0000

    PIG-761: ERROR 2086 on simple JOIN
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/pig/trunk@893825 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/plans/POPackageAnnotator.java
+++ src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/plans/POPackageAnnotator.java
@@ -105,6 +105,8 @@ public class POPackageAnnotator extends MROpPlanVisitor {
             List<MapReduceOper> preds = this.mPlan.getPredecessors(mr);
             for (Iterator<MapReduceOper> it = preds.iterator(); it.hasNext();) {
                 MapReduceOper mrOper = it.next();
+                if (mrOper.isLimitOnly())
+                    mrOper = this.mPlan.getPredecessors(mrOper).get(0);
                 lrFound += patchPackage(mrOper.reducePlan, pkg);
                 if(lrFound == pkg.getNumInps()) {
                     break;
