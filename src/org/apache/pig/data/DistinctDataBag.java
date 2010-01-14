ec6e5e09aab5c7a89826ea80bc6e064402d645c4
Jianyong Dai <daijy@apache.org>
2010-01-14 23:38:09 +0000

    PIG-1185: Data bags do not close spill files after using iterator to read tuples
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/pig/branches/branch-0.6@899475 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/data/DistinctDataBag.java
+++ src/org/apache/pig/data/DistinctDataBag.java
@@ -430,6 +430,11 @@ public class DistinctDataBag extends DefaultAbstractBag {
                         // Out of tuples in this file.  Set our slot in the
                         // array to null so we don't keep trying to read from
                         // this file.
+                        try {
+                            in.close();
+                        }catch(IOException e) {
+                            log.warn("Failed to close spill file.", e);
+                        }
                         mStreams.set(fileNum, null);
                         return;
                     } catch (IOException ioe) {
@@ -518,6 +523,7 @@ public class DistinctDataBag extends DefaultAbstractBag {
                             t.write(out);
                         }
                         out.flush();
+                        out.close();
                     } catch (IOException ioe) {
                         String msg = "Unable to find our spill file.";
                         log.fatal(msg, ioe);
--- src/org/apache/pig/data/SortedDataBag.java
+++ src/org/apache/pig/data/SortedDataBag.java
@@ -393,6 +393,11 @@ public class SortedDataBag extends DefaultAbstractBag{
                     // Out of tuples in this file.  Set our slot in the
                     // array to null so we don't keep trying to read from
                     // this file.
+                    try {
+                        in.close();
+                    }catch(IOException e) {
+                        log.warn("Failed to close spill file.", e);
+                    }                	
                     mStreams.set(fileNum, null);
                 } catch (IOException ioe) {
                     String msg = "Unable to find our spill file.";
@@ -477,6 +482,7 @@ public class SortedDataBag extends DefaultAbstractBag{
                             t.write(out);
                         }
                         out.flush();
+                        out.close();
                     } catch (IOException ioe) {
                         String msg = "Unable to find our spill file.";
                         log.fatal(msg, ioe);
