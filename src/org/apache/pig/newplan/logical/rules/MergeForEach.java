24609f4994e6ac4b25a61bdb51b05a847a29d2dc
daijy <daijy@13f79535-47bb-0310-9956-ffa450edef68>
2010-09-29 05:26:33 +0000

    PIG-1637: Combiner not use because optimizor inserts a foreach between group and algebric function
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/pig/trunk@1002475 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/newplan/logical/rules/MergeForEach.java
+++ src/org/apache/pig/newplan/logical/rules/MergeForEach.java
@@ -93,19 +93,43 @@ public class MergeForEach extends Rule {
             // Otherwise, we may do expression calculation more than once, defeat the benefit of this
             // optimization
             Set<Integer> inputs = new HashSet<Integer>();
+            boolean duplicateInputs = false;
             for (Operator op : foreach2.getInnerPlan().getSources()) {
                 // If the source is not LOInnerLoad, then it must be LOGenerate. This happens when 
                 // the 1st ForEach does not rely on any input of 2nd ForEach
                 if (op instanceof LOInnerLoad) {
                     LOInnerLoad innerLoad = (LOInnerLoad)op;
                     int input = innerLoad.getProjection().getColNum();
-                    if (inputs.contains(input))
-                        return false;
+                    if (inputs.contains(input)) {
+                        duplicateInputs = true;
+                        break;
+                    }
                     else
                         inputs.add(input);
                     
                     if (innerLoad.getProjection().isProjectStar())
                         return false;
+                }
+            }
+            
+            // Duplicate inputs in the case first foreach only containing LOInnerLoad and
+            // LOGenerate is allowed, and output plan is simple projection
+            if (duplicateInputs) {
+                Iterator<Operator> it1 = foreach1.getInnerPlan().getOperators();
+                while( it1.hasNext() ) {
+                    Operator op = it1.next();
+                    if(!(op instanceof LOGenerate) && !(op instanceof LOInnerLoad))
+                        return false;
+                    if (op instanceof LOGenerate) {
+                        List<LogicalExpressionPlan> outputPlans = ((LOGenerate)op).getOutputPlans();
+                        for (LogicalExpressionPlan outputPlan : outputPlans) {
+                            Iterator<Operator> iter = outputPlan.getOperators();
+                            while (iter.hasNext()) {
+                                if (!(iter.next() instanceof ProjectExpression))
+                                    return false;
+                            }
+                        }
+                    }
                 }
             }
             
@@ -117,25 +141,40 @@ public class MergeForEach extends Rule {
             return subPlan;
         }
 
-        private void addBranchToPlan(LOGenerate gen, int branch, OperatorPlan newPlan) {
+        // If op is LOInnerLoad, get a copy of it, otherwise, return op itself
+        private Operator getOperatorToMerge(Operator op, OperatorPlan newPlan, LOForEach newForEach) {
+            Operator opToMerge = op;
+            if (op instanceof LOInnerLoad) {
+                opToMerge = new LOInnerLoad(newPlan, newForEach, ((LOInnerLoad)op).getColNum());
+            } else {
+                opToMerge.setPlan(newPlan);
+            }
+            return opToMerge;
+        }
+        
+        private Operator addBranchToPlan(LOGenerate gen, int branch, OperatorPlan newPlan, LOForEach newForEach) {
+            Operator opNextToGen;
             Operator op = gen.getPlan().getPredecessors(gen).get(branch);
-            newPlan.add(op);
-            op.setPlan(newPlan);
+            Operator opToMerge = getOperatorToMerge(op, newPlan, newForEach);
+            newPlan.add(opToMerge);
+            opNextToGen = opToMerge;
+            
             Operator pred;
             if (gen.getPlan().getPredecessors(op)!=null)
                 pred = gen.getPlan().getPredecessors(op).get(0);
             else
                 pred = null;
             while (pred!=null) {
-                newPlan.add(pred);
-                pred.setPlan(newPlan);
-                newPlan.connect(pred, op);
+                Operator predToMerge = getOperatorToMerge(pred, newPlan, newForEach);
+                newPlan.add(predToMerge);
+                newPlan.connect(predToMerge, op);
                 op = pred;
                 if (gen.getPlan().getPredecessors(pred)!=null)
                     pred = gen.getPlan().getPredecessors(pred).get(0);
                 else
                     pred = null;
             }
+            return opNextToGen;
         }
         
         @Override
@@ -159,7 +198,8 @@ public class MergeForEach extends Rule {
             
             for (LogicalExpressionPlan exp2 : gen2.getOutputPlans()) {
                 LogicalExpressionPlan newExpPlan = new LogicalExpressionPlan();
-                newExpPlan.merge(exp2);
+                LogicalExpressionPlan exp2Copy = exp2.deepCopy();
+                newExpPlan.merge(exp2Copy);
                 
                 // Add expression plan in 2nd ForEach
                 List<Operator> exp2Sinks = new ArrayList<Operator>();
@@ -171,7 +211,8 @@ public class MergeForEach extends Rule {
                         LOInnerLoad innerLoad = (LOInnerLoad)foreach2.getInnerPlan().getPredecessors(gen2).get(proj.getInputNum());
                         int exp1Pos = innerLoad.getProjection().getColNum();
                         LogicalExpressionPlan exp1 = gen1.getOutputPlans().get(exp1Pos);
-                        List<Operator> exp1Sources = newExpPlan.merge(exp1);
+                        LogicalExpressionPlan exp1Copy = exp1.deepCopy();
+                        List<Operator> exp1Sources = newExpPlan.merge(exp1Copy);
                         
                         // Copy expression plan to the new ForEach, connect to the expression plan of 2nd ForEach
                         Operator exp1Source = exp1Sources.get(0);
@@ -191,8 +232,7 @@ public class MergeForEach extends Rule {
                 List<Operator> exp1Sinks = newExpPlan.getSinks();
                 for (Operator exp1Sink : exp1Sinks) {
                     if (exp1Sink instanceof ProjectExpression) {
-                        addBranchToPlan(gen1, ((ProjectExpression)exp1Sink).getInputNum(), newForEachInnerPlan);
-                        Operator opNextToGen = foreach1.getInnerPlan().getPredecessors(gen1).get(((ProjectExpression)exp1Sink).getInputNum());
+                        Operator opNextToGen = addBranchToPlan(gen1, ((ProjectExpression)exp1Sink).getInputNum(), newForEachInnerPlan, newForEach);
                         newForEachInnerPlan.connect(opNextToGen, newGen);
                         int input = newForEachInnerPlan.getPredecessors(newGen).indexOf(opNextToGen);
                         ((ProjectExpression)exp1Sink).setInputNum(input);
