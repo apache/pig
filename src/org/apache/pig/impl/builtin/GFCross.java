15f015374d4280eff8cc94ea9e4906ad2495d64d
Alan Gates <gates@apache.org>
2011-03-30 02:34:22 +0000

    PIG-1932: GFCross should allow the user to set the DEFAULT_PARALLELISM value.
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1086824 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/impl/builtin/GFCross.java
+++ src/org/apache/pig/impl/builtin/GFCross.java
@@ -20,36 +20,54 @@ package org.apache.pig.impl.builtin;
 import java.io.IOException;
 import java.util.Random;
 
+import org.apache.hadoop.conf.Configuration;
+
 import org.apache.pig.EvalFunc;
 import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.data.BagFactory;
 import org.apache.pig.data.DataBag;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.util.UDFContext;
 
 
 public class GFCross extends EvalFunc<DataBag> {
-    int numInputs, myNumber, numGroupsPerInput;
-    BagFactory mBagFactory = BagFactory.getInstance();
-    TupleFactory mTupleFactory = TupleFactory.getInstance();
+    private int numInputs, myNumber, numGroupsPerInput, numGroupsGoingTo;
+    private BagFactory mBagFactory = BagFactory.getInstance();
+    private TupleFactory mTupleFactory = TupleFactory.getInstance();
+    private int parallelism = 0;
+    private Random r = new Random();
+
+    static private final int DEFAULT_PARALLELISM = 96;
     
-    public static final int DEFAULT_PARALLELISM = 96;
 
     @Override
     public DataBag exec(Tuple input) throws IOException {
-        try{
+        if (parallelism == 0) {
+            parallelism = DEFAULT_PARALLELISM;
+            Configuration cfg = UDFContext.getUDFContext().getJobConf();
+            if (cfg != null) {
+                String s = cfg.get("mapred.reduce.tasks");
+                if (s == null) {
+                    throw new IOException("Unable to determine parallelism from job conf");
+                }
+                parallelism = Integer.valueOf(s);
+            }
+
             numInputs = (Integer)input.get(0);
             myNumber = (Integer)input.get(1);
         
-            DataBag output = mBagFactory.newDefaultBag();
-            
-            numGroupsPerInput = (int) Math.ceil(Math.pow(DEFAULT_PARALLELISM, 1.0/numInputs));
-            int numGroupsGoingTo = (int) Math.pow(numGroupsPerInput,numInputs - 1);
-                
+            numGroupsPerInput = (int) Math.ceil(Math.pow(parallelism, 1.0/numInputs));
+            numGroupsGoingTo = (int) Math.pow(numGroupsPerInput,numInputs - 1);
+        }
+
+        DataBag output = mBagFactory.newDefaultBag();
+
+        try{
+               
             int[] digits = new int[numInputs];
             for (int i=0; i<numInputs; i++){
                 if (i == myNumber){
-                    Random r = new Random(System.currentTimeMillis());
                     digits[i] = r.nextInt(numGroupsPerInput);
                 }else{
                     digits[i] = 0;
