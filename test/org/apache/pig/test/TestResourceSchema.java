4a2aa437206e58c788f1dbeca5ea19c9b249cee5
Alan Gates <gates@apache.org>
2011-06-20 17:06:14 +0000

    PIG-2112: ResourceSchema.toString does not properly handle maps in the schema
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1137707 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/test/TestResourceSchema.java
+++ test/org/apache/pig/test/TestResourceSchema.java
@@ -19,6 +19,7 @@
 package org.apache.pig.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -36,6 +37,7 @@ import org.apache.pig.data.DataType;
 import org.apache.pig.impl.logicalLayer.FrontendException;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
 import org.apache.pig.impl.logicalLayer.schema.SchemaMergeException;
+import org.apache.pig.impl.util.Utils;
 import org.apache.pig.test.utils.TypeCheckingTestUtil;
 import org.junit.Test;
 
@@ -269,5 +271,125 @@ public class TestResourceSchema {
         } catch (FrontendException e) {
             assertTrue(e.getErrorCode()==2218);
         }
+    }
+
+    /**
+     * Test that we can turn a schema into a string and then parse it
+     * back into a schema.
+     */
+    @Test
+    public void testToStringAndParse() throws Exception {
+        ResourceSchema rs = new ResourceSchema();
+        ResourceFieldSchema[] fields = new ResourceFieldSchema[11];
+
+        byte[] types = {DataType.INTEGER, DataType.LONG, DataType.FLOAT,
+            DataType.DOUBLE, DataType.BYTEARRAY, DataType.CHARARRAY,
+            DataType.MAP, DataType.TUPLE, DataType.TUPLE, DataType.BAG,
+            DataType.BAG};
+        String[] names = {"i", "l", "f",
+            "d", "b", "s",
+            "m", "tschema", "tnull", "bschema",
+            "bnull"};
+        
+        for (int i = 0; i < fields.length; i++) {
+            fields[i] = new ResourceFieldSchema();
+            fields[i].setName(names[i]);
+            fields[i].setType(types[i]);
+        }
+
+        // Add in the schemas for the tuple and the bag
+        ResourceSchema tschema = new ResourceSchema();
+        ResourceFieldSchema[] tfields = new ResourceFieldSchema[3];
+        for (int i = 0; i < 3; i++) {
+            tfields[i] = new ResourceFieldSchema();
+            tfields[i].setName(names[i]);
+            tfields[i].setType(types[i]);
+        }
+        tschema.setFields(tfields);
+        fields[7].setSchema(tschema);
+
+        ResourceSchema bschema = new ResourceSchema();
+        ResourceFieldSchema[] bfields = new ResourceFieldSchema[1];
+        bfields[0] = new ResourceFieldSchema();
+        bfields[0].setName("t");
+        bfields[0].setType(DataType.TUPLE);
+
+        ResourceSchema tbschema = new ResourceSchema();
+        ResourceFieldSchema[] tbfields = new ResourceFieldSchema[3];
+        for (int i = 3; i < 6; i++) {
+            tbfields[i-3] = new ResourceFieldSchema();
+            tbfields[i-3].setName(names[i]);
+            tbfields[i-3].setType(types[i]);
+        }
+        tbschema.setFields(tbfields);
+
+        bfields[0].setSchema(tbschema);
+        bschema.setFields(bfields);
+
+        fields[9].setSchema(bschema);
+
+        rs.setFields(fields);
+
+        String strSchema = rs.toString();
+
+        assertEquals("i:int,l:long,f:float,d:double,b:bytearray,s:" +
+            "chararray,m:[],tschema:(i:int,l:long,f:float)," +
+            "tnull:(),bschema:{t:(d:double,b:bytearray,s:chararray)},bnull:{}",
+            strSchema);
+
+        ResourceSchema after =
+            new ResourceSchema(Utils.getSchemaFromString(strSchema));
+        ResourceFieldSchema[] afterFields = after.getFields();
+
+        assertEquals(11, afterFields.length);
+        assertEquals("i", afterFields[0].getName());
+        assertEquals(DataType.INTEGER, afterFields[0].getType());
+        assertEquals("l", afterFields[1].getName());
+        assertEquals(DataType.LONG, afterFields[1].getType());
+        assertEquals("f", afterFields[2].getName());
+        assertEquals(DataType.FLOAT, afterFields[2].getType());
+        assertEquals("d", afterFields[3].getName());
+        assertEquals(DataType.DOUBLE, afterFields[3].getType());
+        assertEquals("b", afterFields[4].getName());
+        assertEquals(DataType.BYTEARRAY, afterFields[4].getType());
+        assertEquals("s", afterFields[5].getName());
+        assertEquals(DataType.CHARARRAY, afterFields[5].getType());
+        assertEquals("m", afterFields[6].getName());
+        assertEquals(DataType.MAP, afterFields[6].getType());
+        assertEquals("tschema", afterFields[7].getName());
+        assertEquals(DataType.TUPLE, afterFields[7].getType());
+        assertEquals("tnull", afterFields[8].getName());
+        assertEquals(DataType.TUPLE, afterFields[8].getType());
+        assertEquals("bschema", afterFields[9].getName());
+        assertEquals(DataType.BAG, afterFields[9].getType());
+        assertEquals("bnull", afterFields[10].getName());
+        assertEquals(DataType.BAG, afterFields[10].getType());
+
+        assertNotNull(afterFields[7].getSchema());
+        ResourceFieldSchema[] tAfterFields =
+            afterFields[7].getSchema().getFields();
+        assertEquals(3, tAfterFields.length);
+        assertEquals("i", tAfterFields[0].getName());
+        assertEquals(DataType.INTEGER, tAfterFields[0].getType());
+        assertEquals("l", tAfterFields[1].getName());
+        assertEquals(DataType.LONG, tAfterFields[1].getType());
+        assertEquals("f", tAfterFields[2].getName());
+        assertEquals(DataType.FLOAT, tAfterFields[2].getType());
+
+        assertNotNull(afterFields[9].getSchema());
+        ResourceFieldSchema[] bAfterFields =
+            afterFields[9].getSchema().getFields();
+        assertEquals(1, bAfterFields.length);
+        assertNotNull(bAfterFields[0].getSchema());
+
+        ResourceFieldSchema[] tbAfterFields =
+            bAfterFields[0].getSchema().getFields();
+        assertEquals(3, tbAfterFields.length);
+        assertEquals("d", tbAfterFields[0].getName());
+        assertEquals(DataType.DOUBLE, tbAfterFields[0].getType());
+        assertEquals("b", tbAfterFields[1].getName());
+        assertEquals(DataType.BYTEARRAY, tbAfterFields[1].getType());
+        assertEquals("s", tbAfterFields[2].getName());
+        assertEquals(DataType.CHARARRAY, tbAfterFields[2].getType());
     }
 }
