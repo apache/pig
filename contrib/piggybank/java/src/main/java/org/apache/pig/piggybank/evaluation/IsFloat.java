3da187e333143e35af1ec0e931157f0b40c91781
Jianyong Dai <daijy@apache.org>
2012-01-03 04:46:45 +0000

    PIG-2443: [Piggybank] Add UDFs to check if a String is an Integer And if a String is Numeric
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1226642 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/piggybank/java/src/main/java/org/apache/pig/piggybank/evaluation/IsFloat.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pig.piggybank.evaluation;
+
+import java.io.IOException;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.PigWarning;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+
+/**
+ * This UDF is used to check whether the String input is a Float.
+ * Note this function checks for Float range.
+ * If range is not important, use IsNumeric instead if you would like to check if a String is numeric. 
+ * Also IsNumeric performs slightly better compared to this function.
+ */
+
+public class IsFloat extends EvalFunc<Boolean> {
+    @Override
+    public Boolean exec(Tuple input) throws IOException {
+        if (input == null || input.size() == 0) return false;
+        try {
+            String str = (String)input.get(0);
+            if (str == null || str.length() == 0) return false;
+            Float.parseFloat(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        } catch (ClassCastException e) {
+            warn("Unable to cast input "+input.get(0)+" of class "+
+                    input.get(0).getClass()+" to String", PigWarning.UDF_WARNING_1);
+            return false;
+        }
+
+        return true;
+    }
+    
+    @Override
+    public Schema outputSchema(Schema input) {
+        return new Schema(new Schema.FieldSchema(null, DataType.BOOLEAN)); 
+    }
+}
+
