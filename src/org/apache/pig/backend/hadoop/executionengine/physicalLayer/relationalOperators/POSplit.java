2dbea07bc3ec830dc89cfdfc5449f19e3bb3ba6e
Jianyong Dai <daijy@apache.org>
2012-03-23 01:43:13 +0000

    PIG-2442: Multiple Stores in pig streaming causes infinite waiting
    git-svn-id: https://svn.apache.org/repos/asf/pig/branches/branch-0.10@1304163 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/backend/hadoop/executionengine/physicalLayer/relationalOperators/POSplit.java
+++ src/org/apache/pig/backend/hadoop/executionengine/physicalLayer/relationalOperators/POSplit.java
@@ -291,7 +291,7 @@ public class POSplit extends PhysicalOperator {
             } 
             
             int idx = processedSet.nextClearBit(0);
-            if (inpEOP && parentPlan.endOfAllInput) {
+            if (inpEOP ) {
                 myPlans.get(idx).endOfAllInput = true;
             }
             PhysicalOperator leaf = myPlans.get(idx).getLeaves().get(0);
