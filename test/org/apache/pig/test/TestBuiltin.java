68ec9013e14b9263662ad0c4561c372d6613a5cd
Jianyong Dai <daijy@apache.org>
2012-09-25 00:57:20 +0000

    PIG-2930: ant test doesn't compile in trunk
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1389668 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/test/TestBuiltin.java
+++ test/org/apache/pig/test/TestBuiltin.java
@@ -32,6 +32,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Random;
 import java.util.Set;
 import java.util.StringTokenizer;
