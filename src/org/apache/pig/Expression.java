2bbea70caf31c88f1fa9294718afe577347e1ea4
Jonathan Coveney <jcoveney@apache.org>
2012-10-18 23:19:01 +0000

    PIG-1283: COUNT on null bag causes failure (anand via jcoveney)
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1399916 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/Expression.java
+++ src/org/apache/pig/Expression.java
@@ -45,6 +45,7 @@ public abstract class Expression {
         OP_GE(" >= "),
         OP_LT(" < "),
         OP_LE(" <= "),
+        OP_MATCH(" matches "),
 
         //binary logical
         OP_AND(" and "),
