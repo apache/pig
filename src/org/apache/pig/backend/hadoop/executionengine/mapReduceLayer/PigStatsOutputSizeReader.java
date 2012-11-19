ca1ec37169a96f89b50265ddac5badb96128aedf
Cheolsoo Park <cheolsoo@apache.org>
2012-11-19 19:53:50 +0000

    PIG-2924 PigStats should not be assuming all Storage classes to be file-based storage
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1411373 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/PigStatsOutputSizeReader.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pig.backend.hadoop.executionengine.mapReduceLayer;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.pig.backend.hadoop.executionengine.physicalLayer.relationalOperators.POStore;
+import org.apache.pig.classification.InterfaceAudience;
+import org.apache.pig.classification.InterfaceStability;
+
+/**
+ * Interface to implement when you want to customize the way of computing the
+ * size of output in PigStats. Since not every storage is file-based (e.g.
+ * HBaseStorage), the output size cannot always be computed as the total size of
+ * output files.
+ *
+ * @see FileBasedOutputSizeReader 
+ */
+@InterfaceAudience.Public
+@InterfaceStability.Evolving
+public interface PigStatsOutputSizeReader {
+
+    static final String OUTPUT_SIZE_READER_KEY = "pig.stats.output.size.reader";
+
+    /** 
+     * Returns whether the given PSStore is supported by this output size reader
+     * or not.
+     * @param sto POStore
+     */
+    public boolean supports(POStore sto);
+
+    /**
+     * Returns the size of output in bytes. If the size of output cannot be
+     * computed for any reason, -1 should be returned.
+     * @param sto POStore
+     * @param conf configuration
+     */
+    public long getOutputSize(POStore sto, Configuration conf) throws IOException;
+}
