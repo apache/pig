122852c3ca2d2b50268c85a6f02b66fc5b50ed38
William W. Graham Jr <billgraham@apache.org>
2012-11-20 07:18:41 +0000

    PIG-2934: HBaseStorage filter optimizations (billgraham)
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1411568 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/backend/hadoop/hbase/HBaseStorage.java
+++ src/org/apache/pig/backend/hadoop/hbase/HBaseStorage.java
@@ -53,8 +53,9 @@ import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp;
 import org.apache.hadoop.hbase.filter.FamilyFilter;
 import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.filter.FilterList;
-import org.apache.hadoop.hbase.filter.QualifierFilter;
 import org.apache.hadoop.hbase.filter.RowFilter;
+import org.apache.hadoop.hbase.filter.WhileMatchFilter;
+import org.apache.hadoop.hbase.filter.QualifierFilter;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
@@ -112,6 +113,12 @@ import com.google.common.collect.Lists;
  * name with a trailing '*'. For example passing <code>friends:bob_*</code> to
  * the constructor in the above example would cause only columns that start with
  * <i>bob_</i> to be loaded.
+ * <P>
+ * Note that when using a prefix like <code>friends:bob_*</code>, explicit HBase filters are set for
+ * all columns and prefixes specified. Querying HBase with many filters can cause performance
+ * degredation. This is typically seen when mixing one or more prefixed descriptors with a large list
+ * of columns. In that case better perfomance will be seen by either loading the entire family via
+ * <code>friends:*</code> or by specifying explicit column descriptor names.
  * <P>
  * Below is an example showing how to store data into HBase:
  * <pre>{@code
@@ -234,6 +241,7 @@ public class HBaseStorage extends LoadFunc implements StoreFuncInterface, LoadPu
      * <li>-minTimestamp= Scan's timestamp for min timeRange
      * <li>-maxTimestamp= Scan's timestamp for max timeRange
      * <li>-timestamp= Scan's specified timestamp
+     * <li>-caster=(HBaseBinaryConverter|Utf8StorageConverter) Utf8StorageConverter is the default
      * To be used with extreme caution, since this could result in data loss
      * (see http://hbase.apache.org/book.html#perf.hbase.client.putwal).
      * </ul>
@@ -309,7 +317,7 @@ public class HBaseStorage extends LoadFunc implements StoreFuncInterface, LoadPu
             timestamp_ = 0;
         }
         
-        initScan();	    
+        initScan();
     }
 
     /**
@@ -376,70 +384,146 @@ public class HBaseStorage extends LoadFunc implements StoreFuncInterface, LoadPu
         if (configuredOptions_.hasOption("gt")) {
             gt_ = Bytes.toBytesBinary(Utils.slashisize(configuredOptions_.getOptionValue("gt")));
             addRowFilter(CompareOp.GREATER, gt_);
+            scan.setStartRow(gt_);
         }
         if (configuredOptions_.hasOption("lt")) {
             lt_ = Bytes.toBytesBinary(Utils.slashisize(configuredOptions_.getOptionValue("lt")));
             addRowFilter(CompareOp.LESS, lt_);
+            scan.setStopRow(lt_);
         }
         if (configuredOptions_.hasOption("gte")) {
             gte_ = Bytes.toBytesBinary(Utils.slashisize(configuredOptions_.getOptionValue("gte")));
-            addRowFilter(CompareOp.GREATER_OR_EQUAL, gte_);
+            scan.setStartRow(gte_);
         }
         if (configuredOptions_.hasOption("lte")) {
             lte_ = Bytes.toBytesBinary(Utils.slashisize(configuredOptions_.getOptionValue("lte")));
-            addRowFilter(CompareOp.LESS_OR_EQUAL, lte_);
+            byte[] lt = increment(lte_);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(String.format("Incrementing lte value of %s from bytes %s to %s to set stop row",
+                          Bytes.toString(lte_), toString(lte_), toString(lt)));
+            }
+
+            if (lt != null) {
+                scan.setStopRow(increment(lte_));
+            }
+
+            // The WhileMatchFilter will short-circuit the scan after we no longer match. The
+            // setStopRow call will limit the number of regions we need to scan
+            addFilter(new WhileMatchFilter(new RowFilter(CompareOp.LESS_OR_EQUAL, new BinaryComparator(lte_))));
         }
         if (configuredOptions_.hasOption("minTimestamp") || configuredOptions_.hasOption("maxTimestamp")){
             scan.setTimeRange(minTimestamp_, maxTimestamp_);
         }
         if (configuredOptions_.hasOption("timestamp")){
-        	scan.setTimeStamp(timestamp_);
+            scan.setTimeStamp(timestamp_);
         }
-        // apply any column filters
+
+        // if the group of columnInfos for this family doesn't contain a prefix, we don't need
+        // to set any filters, we can just call addColumn or addFamily. See javadocs below.
+        boolean columnPrefixExists = false;
+        for (ColumnInfo columnInfo : columnInfo_) {
+            if (columnInfo.getColumnPrefix() != null) {
+                columnPrefixExists = true;
+                break;
+            }
+        }
+
+        if (!columnPrefixExists) {
+            addFiltersWithoutColumnPrefix(columnInfo_);
+        }
+        else {
+            addFiltersWithColumnPrefix(columnInfo_);
+        }
+    }
+
+    /**
+     * If there is no column with a prefix, we don't need filters, we can just call addColumn and
+     * addFamily on the scan
+     */
+    private void addFiltersWithoutColumnPrefix(List<ColumnInfo> columnInfos) {
+        for (ColumnInfo columnInfo : columnInfos) {
+            if (columnInfo.columnName != null) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Adding column to scan via addColumn with cf:name = " +
+                            Bytes.toString(columnInfo.getColumnFamily()) + ":" +
+                            Bytes.toString(columnInfo.getColumnName()));
+                }
+                scan.addColumn(columnInfo.getColumnFamily(), columnInfo.getColumnName());
+            }
+            else {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Adding column family to scan via addFamily with cf:name = " +
+                            Bytes.toString(columnInfo.getColumnFamily()));
+                }
+                scan.addFamily(columnInfo.getColumnFamily());
+            }
+        }
+    }
+
+    /**
+     *  If we have a qualifier with a prefix and a wildcard (i.e. cf:foo*), we need a filter on every
+     *  possible column to be returned as shown below. This will become very inneficient for long
+     *  lists of columns mixed with a prefixed wildcard.
+     *
+     *  FilterList - must pass ALL of
+     *   - FamilyFilter
+     *   - AND a must pass ONE FilterList of
+     *    - either Qualifier
+     *    - or ColumnPrefixFilter
+     *
+     *  If we have only column family filters (i.e. cf:*) or explicit column descriptors
+     *  (i.e., cf:foo) or a mix of both then we don't need filters, since the scan will take
+     *  care of that.
+     */
+    private void addFiltersWithColumnPrefix(List<ColumnInfo> columnInfos) {
+        // we need to apply a CF AND column list filter for each family
         FilterList allColumnFilters = null;
-        for (ColumnInfo colInfo : columnInfo_) {
-            // all column family filters roll up to one parent OR filter
+        Map<String, List<ColumnInfo>> groupedMap = groupByFamily(columnInfos);
+        for (String cfString : groupedMap.keySet()) {
+            List<ColumnInfo> columnInfoList = groupedMap.get(cfString);
+            byte[] cf = Bytes.toBytes(cfString);
+
+            // all filters roll up to one parent OR filter
             if (allColumnFilters == null) {
                 allColumnFilters = new FilterList(FilterList.Operator.MUST_PASS_ONE);
             }
 
-            // and each filter contains a column family filter
-            FilterList thisColumnFilter = new FilterList(FilterList.Operator.MUST_PASS_ALL);
-            thisColumnFilter.addFilter(new FamilyFilter(CompareOp.EQUAL,
-                    new BinaryComparator(colInfo.getColumnFamily())));
+            // each group contains a column family filter AND (all) and an OR (one of) of
+            // the column filters
+            FilterList thisColumnGroupFilter = new FilterList(FilterList.Operator.MUST_PASS_ALL);
+            thisColumnGroupFilter.addFilter(new FamilyFilter(CompareOp.EQUAL, new BinaryComparator(cf)));
+            FilterList columnFilters = new FilterList(FilterList.Operator.MUST_PASS_ONE);
+            for (ColumnInfo colInfo : columnInfoList) {
+                if (colInfo.isColumnMap()) {
 
-            if (colInfo.isColumnMap()) {
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Adding family:prefix filters with values " +
+                                Bytes.toString(colInfo.getColumnFamily()) + COLON +
+                                Bytes.toString(colInfo.getColumnPrefix()));
+                    }
 
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("Adding family:prefix filters with values " +
-                        Bytes.toString(colInfo.getColumnFamily()) + COLON +
-                        Bytes.toString(colInfo.getColumnPrefix()));
+                    // add a PrefixFilter to the list of column filters
+                    if (colInfo.getColumnPrefix() != null) {
+                        columnFilters.addFilter(new ColumnPrefixFilter(
+                            colInfo.getColumnPrefix()));
+                    }
                 }
+                else {
 
-                // each column map filter consists of a FamilyFilter AND
-                // optionally a PrefixFilter
-                if (colInfo.getColumnPrefix() != null) {
-                    thisColumnFilter.addFilter(new ColumnPrefixFilter(
-                        colInfo.getColumnPrefix()));
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Adding family:descriptor filters with values " +
+                                Bytes.toString(colInfo.getColumnFamily()) + COLON +
+                                Bytes.toString(colInfo.getColumnName()));
+                    }
+
+                    // add a QualifierFilter to the list of column filters
+                    columnFilters.addFilter(new QualifierFilter(CompareOp.EQUAL,
+                            new BinaryComparator(colInfo.getColumnName())));
                 }
             }
-            else {
-
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("Adding family:descriptor filters with values " +
-                        Bytes.toString(colInfo.getColumnFamily()) + COLON +
-                        Bytes.toString(colInfo.getColumnName()));
-                }
-
-                // each column value filter consists of a FamilyFilter AND
-                // a QualifierFilter
-                thisColumnFilter.addFilter(new QualifierFilter(CompareOp.EQUAL,
-                        new BinaryComparator(colInfo.getColumnName())));
-            }
-
-            allColumnFilters.addFilter(thisColumnFilter);
+            thisColumnGroupFilter.addFilter(columnFilters);
+            allColumnFilters.addFilter(thisColumnGroupFilter);
         }
-
         if (allColumnFilters != null) {
             addFilter(allColumnFilters);
         }
@@ -1035,4 +1119,74 @@ public class HBaseStorage extends LoadFunc implements StoreFuncInterface, LoadPu
         public String toString() { return originalColumnName; }
     }
 
+    /**
+     * Group the list of ColumnInfo objects by their column family and returns a map of CF to its
+     * list of ColumnInfo objects. Using String as key since it implements Comparable.
+     * @param columnInfos the columnInfo list to group
+     * @return a Map of lists, keyed by their column family.
+     */
+    static Map<String, List<ColumnInfo>> groupByFamily(List<ColumnInfo> columnInfos) {
+        Map<String, List<ColumnInfo>> groupedMap = new HashMap<String, List<ColumnInfo>>();
+        for (ColumnInfo columnInfo : columnInfos) {
+            String cf = Bytes.toString(columnInfo.getColumnFamily());
+            List<ColumnInfo> columnInfoList = groupedMap.get(cf);
+            if (columnInfoList == null) {
+                columnInfoList = new ArrayList<ColumnInfo>();
+            }
+            columnInfoList.add(columnInfo);
+            groupedMap.put(cf, columnInfoList);
+        }
+        return groupedMap;
+    }
+
+    static String toString(byte[] bytes) {
+        if (bytes == null) { return null; }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < bytes.length; i++) {
+            if (i > 0) { sb.append("|"); }
+            sb.append(bytes[i]);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Increments the byte array by one for use with setting stopRow. If all bytes in the array are
+     * set to the maximum byte value, then the original array will be returned with a 0 byte appended
+     * to it. This is because HBase compares bytes from left to right. If byte array B is equal to
+     * byte array A, but with an extra byte appended, A will be < B. For example
+     * {@code}A = byte[] {-1}{@code} increments to
+     * {@code}B = byte[] {-1, 0}{@code} and {@code}A < B{@code}
+     * @param bytes array to increment bytes on
+     * @return a copy of the byte array incremented by 1
+     */
+    static byte[] increment(byte[] bytes) {
+        boolean allAtMax = true;
+        for(int i = 0; i < bytes.length; i++) {
+            if((bytes[bytes.length - i - 1] & 0x0ff) != 255) {
+                allAtMax = false;
+                break;
+            }
+        }
+
+        if (allAtMax) {
+            return Arrays.copyOf(bytes, bytes.length + 1);
+        }
+
+        byte[] incremented = bytes.clone();
+        for(int i = bytes.length - 1; i >= 0; i--) {
+            boolean carry = false;
+            int val = bytes[i] & 0x0ff;
+            int total = val + 1;
+            if(total > 255) {
+                carry = true;
+                total %= 256;
+            } else if (total < 0) {
+                carry = true;
+            }
+            incremented[i] = (byte)total;
+            if (!carry) return incremented;
+        }
+        return incremented;
+    }
 }
