8c7dd6f67236d87b398f7e5ff27d8b224a25b189
Jianyong Dai <daijy@apache.org>
2012-12-20 16:25:23 +0000

    Pig unit test fixes for TestGrunt(1), TestStore(2), TestEmptyInputDir(3)
    git-svn-id: https://svn.apache.org/repos/asf/pig/branches/branch-0.10@1424569 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/test/TestStore.java
+++ test/org/apache/pig/test/TestStore.java
@@ -403,7 +403,7 @@ public class TestStore extends junit.framework.TestCase {
                     Properties props = new Properties();                                          
                     props.setProperty(MapRedUtil.FILE_SYSTEM_NAME, "file:///");
                     ps = new PigServer(ExecType.LOCAL, props);
-                    if (Util.isHadoop1_0()) {
+                    if (Util.isHadoop1_x()) {
                         // MAPREDUCE-1447/3563 (LocalJobRunner does not call methods of mapreduce
                         // OutputCommitter) is fixed only in 0.23.1
                         filesToVerify.put(DummyOutputCommitter.FILE_SETUPJOB_CALLED, Boolean.FALSE);
@@ -525,7 +525,7 @@ public class TestStore extends junit.framework.TestCase {
                     // LocalJobRunner does not call abortTask
                     filesToVerify.put(DummyOutputCommitter.FILE_ABORTTASK_CALLED + "1", Boolean.FALSE);
                     filesToVerify.put(DummyOutputCommitter.FILE_ABORTTASK_CALLED + "2", Boolean.FALSE);
-                    if (Util.isHadoop1_0()) {
+                    if (Util.isHadoop1_x()) {
                         // MAPREDUCE-1447/3563 (LocalJobRunner does not call methods of mapreduce
                         // OutputCommitter) is fixed only in 0.23.1
                         filesToVerify.put(DummyOutputCommitter.FILE_SETUPJOB_CALLED + "1", Boolean.FALSE);
