ba6ddc742320c73580eca14063f58c6eca7f5754
Lee T. Tucker <ltucker@verticloud.com>
2013-01-24 14:56:26 -0800

    patch and allow for interal build of pig tarball

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/verticloud/ant-1.8.4/ -Dversion=0.10.1 -Dhadoopversion=23 clean jar jar-withouthadoop
+pushd contrib/zebra
+ant -Dhadoopversion=23
+popd
+pushd contrib/piggybank/java
+ant -Dhadoopversion=23
+popd
+ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/verticloud/ant-1.8.4/ -Dversion=0.10.1 -Dhadoopversion=23 tar
