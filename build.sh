a8185310bbb0493594f77aab5c31655b9747d02b
Lee T. Tucker <ltucker@verticloud.com>
2013-01-27 08:50:11 -0800

    Update the package to conform to new standards

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh -e
 export DATE_STRING=`date +"%Y%m%d%H%M"`
 export RPM_VERSION=0
-export RPM_NAME=`echo verticloud-pig_${VERSION}`
+export RPM_NAME=`echo vcc-pig_${VERSION}`
 echo "Building Pig Version ${VERSION} to RPM version ${RPM_VERSION}-${DATE_STRING} with RPM name ${RPM_NAME}"
 
 rm -rf ${WORKSPACE}/install-*
-ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/verticloud/ant-1.8.4/ -Dversion=${VERSION} -Dhadoopversion=23 clean jar jar-withouthadoop
+ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/ant-1.8.4/ -Dversion=${VERSION} -Dhadoopversion=23 clean jar jar-withouthadoop
 pushd contrib/zebra
 ant -Dhadoopversion=23
 popd
 pushd contrib/piggybank/java
 ant -Dhadoopversion=23
 popd
-ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/verticloud/ant-1.8.4/ -Dversion=${VERSION} -Dhadoopversion=23 tar
+ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/ant-1.8.4/ -Dversion=${VERSION} -Dhadoopversion=23 tar
 export RPM_BUILD_DIR=${WORKSPACE}/install-${BUILD_NUMBER}/opt
 mkdir --mode=0755 -p ${RPM_BUILD_DIR}
 cd ${RPM_BUILD_DIR}
