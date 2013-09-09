1e8bb97fbf555581bc570117c70543b5b9a3c040
Jianyong Dai <daijy@apache.org>
2013-09-09 17:44:49 +0000

    PIG-3431: Return more information for parsing related exceptions.
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1521222 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/pig/tools/parameters/ParameterSubstitutionException.java
@@ -0,0 +1,15 @@
+package org.apache.pig.tools.parameters;
+
+
+public class ParameterSubstitutionException extends ParseException {
+    static final long serialVersionUID = 1L;
+
+    public ParameterSubstitutionException(String msg) {
+        super(msg);
+    }
+    
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
