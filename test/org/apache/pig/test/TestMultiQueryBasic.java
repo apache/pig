2d84d70e536339b2cf24dee6432d3cdb412b8606
Koji Noguchi <knoguchi@apache.org>
2013-09-25 18:23:31 +0000

    PIG-3458: ScalarExpression lost with multiquery optimization (knoguchi)
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1526254 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/test/TestMultiQueryBasic.java
+++ test/org/apache/pig/test/TestMultiQueryBasic.java
@@ -577,6 +577,44 @@ public class TestMultiQueryBasic {
         Util.deleteFile(new PigContext(ExecType.LOCAL, new Properties()), "output1_checkOutputSpec_test");
         Util.deleteFile(new PigContext(ExecType.LOCAL, new Properties()), "output2_checkOutputSpec_test");
     }
+
+    /**
+     * Test that MultiQuery optimization won't use user's output for
+     * ScalarExpression (and get empty output) at the end
+     */
+    @Test
+    public void testMultiQueryWithScalarExpression() throws Exception {
+
+        System.out.println("===== multi-query with ScalarExpression =====");
+
+        String[] inputData = {"john","henry", "adam"};
+        Util.createLocalInputFile("queryInput.txt", inputData);
+
+        myPig.setBatchOn();
+
+        myPig.registerQuery("a = load 'queryInput.txt' using PigStorage() as (uname:chararray);");
+        myPig.registerQuery("b = group a ALL;");
+        myPig.registerQuery("c = foreach b generate COUNT(a) as count;");
+        myPig.registerQuery("store c into 'output1';");
+        myPig.registerQuery("z = load 'queryInput.txt' using PigStorage() as (uname:chararray);");
+        myPig.registerQuery("y = foreach z generate uname, c.count;");
+        myPig.registerQuery("store y into 'output2';");
+
+        List<ExecJob> jobs = myPig.executeBatch();
+
+        for (ExecJob job : jobs) {
+            assertTrue(job.getStatus() == ExecJob.JOB_STATUS.COMPLETED);
+        }
+        myPig.registerQuery("aa = load 'output2' as (uname:chararray, cnt:int) ;");
+        Iterator<Tuple> it = myPig.openIterator("aa");
+        int i = 0;
+        while(it.hasNext()) {
+            Tuple t = it.next();
+            i++;
+            assertEquals(3, t.get(1));
+        }
+        assertEquals(3, i);
+    }
         
     private static final String DUMMY_STORE_WITH_OUTPUTFORMAT_CLASS
             = "org.apache.pig.test.TestMultiQueryBasic\\$DummyStoreWithOutputFormat";
