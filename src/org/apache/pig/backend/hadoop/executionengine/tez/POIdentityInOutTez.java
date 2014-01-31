fc8df4f1adac35086c7fd5a08ea8238a826ac0b0
Jianyong Dai <daijy@apache.org>
2014-01-31 19:49:44 +0000

    PIG-3732: Use ONE_TO_ONE edge and IdentityInOut in orderby intermediate vertex (add missing file)
    git-svn-id: https://svn.apache.org/repos/asf/pig/branches/tez@1563228 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/pig/backend/hadoop/executionengine/tez/POIdentityInOutTez.java
@@ -0,0 +1,104 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pig.backend.hadoop.executionengine.tez;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.backend.hadoop.executionengine.physicalLayer.Result;
+import org.apache.pig.backend.hadoop.executionengine.physicalLayer.relationalOperators.POLocalRearrange;
+import org.apache.pig.impl.plan.OperatorKey;
+import org.apache.tez.runtime.api.LogicalInput;
+import org.apache.tez.runtime.api.LogicalOutput;
+import org.apache.tez.runtime.library.api.KeyValueReader;
+import org.apache.tez.runtime.library.api.KeyValueWriter;
+
+/**
+ * POIdentityInOutTez is used to pass through tuples as is to next vertex from
+ * previous vertex's POLocalRearrangeTez. For eg: In case of Order By, the
+ * partition vertex which just applies the WeightedRangePartitioner on the
+ * previous vertex data uses POIdentityInOutTez.
+ */
+@InterfaceAudience.Private
+public class POIdentityInOutTez extends POLocalRearrangeTez implements TezLoad, TezOutput {
+
+    private static final long serialVersionUID = 1L;
+    private String inputKey;
+    private transient KeyValueReader reader;
+
+    public POIdentityInOutTez(OperatorKey k, POLocalRearrange inputRearrange) {
+        super(inputRearrange);
+        this.mKey = k;
+    }
+
+    public void setInputKey(String inputKey) {
+        this.inputKey = inputKey;
+    }
+
+    @Override
+    public void attachInputs(Map<String, LogicalInput> inputs,
+            Configuration conf) throws ExecException {
+        LogicalInput input = inputs.get(inputKey);
+        if (input == null) {
+            throw new ExecException("Input from vertex " + inputKey + " is missing");
+        }
+        try {
+            reader = (KeyValueReader) input.getReader();
+        } catch (Exception e) {
+            throw new ExecException(e);
+        }
+    }
+
+    @Override
+    public void attachOutputs(Map<String, LogicalOutput> outputs,
+            Configuration conf) throws ExecException {
+        LogicalOutput output = outputs.get(outputKey);
+        if (output == null) {
+            throw new ExecException("Output to vertex " + outputKey + " is missing");
+        }
+        try {
+            writer = (KeyValueWriter) output.getWriter();
+        } catch (Exception e) {
+            throw new ExecException(e);
+        }
+    }
+
+    @Override
+    public Result getNextTuple() throws ExecException {
+        try {
+            if (!reader.next()) {
+                return RESULT_EOP;
+            } else {
+                writer.write(reader.getCurrentKey(), reader.getCurrentValue());
+            }
+            return RESULT_EMPTY;
+        } catch (IOException e) {
+            throw new ExecException(e);
+        }
+    }
+
+    @Override
+    public String name() {
+        return "POIdentityInOutTez - " + mKey.toString() + "\t->\t " + outputKey;
+    }
+
+}
