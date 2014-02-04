713fe3cc5537075f85ce95409ac9bdd4dbe78af8
Aniket Namadeo Mokashi <aniket486@apache.org>
2014-02-04 22:17:50 +0000

    PIG-3741: Utils.setTmpFileCompressionOnConf can cause side effect for SequenceFileInterStorage (aniket486)
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1564530 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/JobControlCompiler.java
+++ src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/JobControlCompiler.java
@@ -657,6 +657,10 @@ public class JobControlCompiler{
 
             nwJob.setInputFormatClass(PigInputFormat.class);
 
+            // tmp file compression setups
+            // PIG-3741 This must be done before setStoreLocation on POStores
+            Utils.setTmpFileCompressionOnConf(pigContext, conf);
+
             //Process POStore and remove it from the plan
             LinkedList<POStore> mapStores = PlanHelper.getPhysicalOperators(mro.mapPlan, POStore.class);
             LinkedList<POStore> reduceStores = PlanHelper.getPhysicalOperators(mro.reducePlan, POStore.class);
@@ -909,9 +913,6 @@ public class JobControlCompiler{
                 conf.set(PIG_MAP_STORES, ObjectSerializer.serialize(mapStores));
                 conf.set(PIG_REDUCE_STORES, ObjectSerializer.serialize(reduceStores));
             }
-
-            // tmp file compression setups
-            Utils.setTmpFileCompressionOnConf(pigContext, conf);
 
             String tmp;
             long maxCombinedSplitSize = 0;
