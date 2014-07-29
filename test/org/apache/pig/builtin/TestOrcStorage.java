02c42cf6c4bd6b162d58d2d97a2254a58678d213
Rohini Palaniswamy <rohini@apache.org>
2014-07-29 05:06:09 +0000

    PIG-4077: Some fixes and e2e test for OrcStorage (rohini)
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1614252 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/builtin/TestOrcStorage.java
+++ test/org/apache/pig/builtin/TestOrcStorage.java
@@ -18,14 +18,16 @@
 package org.apache.pig.builtin;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -33,22 +35,35 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.ql.io.orc.CompressionKind;
 import org.apache.hadoop.hive.ql.io.orc.OrcFile;
+import org.apache.hadoop.hive.ql.io.orc.OrcStruct;
 import org.apache.hadoop.hive.ql.io.orc.Reader;
 import org.apache.hadoop.hive.ql.io.orc.RecordReader;
+import org.apache.hadoop.hive.serde2.io.ByteWritable;
+import org.apache.hadoop.hive.serde2.io.DoubleWritable;
+import org.apache.hadoop.hive.serde2.io.HiveDecimalWritable;
+import org.apache.hadoop.hive.serde2.io.ShortWritable;
+import org.apache.hadoop.hive.serde2.io.TimestampWritable;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.io.BooleanWritable;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.pig.ExecType;
 import org.apache.pig.PigServer;
 import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.backend.hadoop.datastorage.ConfigurationUtil;
-import org.apache.pig.data.DataBag;
+import org.apache.pig.data.BinSedesTuple;
 import org.apache.pig.data.DataByteArray;
+import org.apache.pig.data.DefaultDataBag;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
 import org.apache.pig.test.Util;
 import org.joda.time.DateTime;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -57,16 +72,16 @@ public class TestOrcStorage {
 
     final private static String basedir = "test/org/apache/pig/builtin/orc/";
     final private static String outbasedir = System.getProperty("user.dir") + "/build/test/TestOrcStorage/";
-    
+
     private static String INPUT1 = outbasedir + "TestOrcStorage_1";
     private static String OUTPUT1 = outbasedir + "TestOrcStorage_2";
     private static String OUTPUT2 = outbasedir + "TestOrcStorage_3";
     private static String OUTPUT3 = outbasedir + "TestOrcStorage_4";
     private static String OUTPUT4 = outbasedir + "TestOrcStorage_5";
-    
+
     private static PigServer pigServer = null;
     private static FileSystem fs;
-    
+
     @Before
     public void setup() throws ExecException, IOException {
         pigServer = new PigServer(ExecType.LOCAL);
@@ -82,7 +97,7 @@ public class TestOrcStorage {
             OUTPUT4 = OUTPUT4.replace("\\", "/");
         }
     }
-    
+
     @After
     public void teardown() throws IOException {
         if(pigServer != null) {
@@ -90,12 +105,12 @@ public class TestOrcStorage {
         }
         deleteTestFiles();
     }
-    
+
     private void generateInputFiles() throws IOException {
         String[] input = {"65536\tworld", "1\thello"};
         Util.createLocalInputFile(INPUT1, input);
     }
-    
+
     private static void deleteTestFiles() throws IOException {
         Util.deleteDirectory(new File(outbasedir));
     }
@@ -109,30 +124,10 @@ public class TestOrcStorage {
                 "middle: (list: {(int1: int,string1: chararray)}),list: {(int1: int,string1: chararray)}," +
                 "map: map[(int1: int,string1: chararray)],ts: datetime,decimal1: bigdecimal}");
         Iterator<Tuple> iter = pigServer.openIterator("A");
-        int count=0;
-        Tuple t;
-        while (iter.hasNext()) {
-            t = (Tuple)iter.next();
-            assertEquals(t.size(), 14);
-            assertTrue(t.get(0) instanceof Boolean);
-            assertTrue(t.get(1) instanceof Integer);
-            assertTrue(t.get(2) instanceof Integer);
-            assertTrue(t.get(3) instanceof Integer);
-            assertTrue(t.get(4) instanceof Long);
-            assertTrue(t.get(5) instanceof Float);
-            assertTrue(t.get(6) instanceof Double);
-            assertTrue(t.get(7) instanceof DataByteArray);
-            assertTrue(t.get(8) instanceof String);
-            assertTrue(t.get(9) instanceof Tuple);
-            assertTrue(t.get(10) instanceof DataBag);
-            assertTrue(t.get(11) instanceof Map);
-            assertTrue(t.get(12) instanceof DateTime);
-            assertTrue(t.get(13) instanceof BigDecimal);
-            count++;
-        }
-        assertEquals(count, 7500);
+
+        verifyData(new Path(basedir + "orc-file-11-format.orc"), iter, fs, 7500);
     }
-    
+
     @Test
     public void testJoinWithPruning() throws Exception {
         pigServer.registerQuery("A = load '" + basedir + "orc-file-11-format.orc'" + " using OrcStorage();" );
@@ -159,7 +154,7 @@ public class TestOrcStorage {
         Path outputFilePath = new Path(new Path(OUTPUT1), "part-m-00000");
         Reader reader = OrcFile.createReader(fs, outputFilePath);
         assertEquals(reader.getNumberOfRows(), 2);
-        
+
         RecordReader rows = reader.rows(null);
         Object row = rows.next(null);
         StructObjectInspector soi = (StructObjectInspector)reader.getObjectInspector();
@@ -169,7 +164,7 @@ public class TestOrcStorage {
                 soi.getAllStructFieldRefs().get(1));
         assertEquals(intWritable.get(), 65536);
         assertEquals(text.toString(), "world");
-        
+
         row = rows.next(null);
         intWritable = (IntWritable)soi.getStructFieldData(row,
                 soi.getAllStructFieldRefs().get(0));
@@ -177,10 +172,10 @@ public class TestOrcStorage {
                 soi.getAllStructFieldRefs().get(1));
         assertEquals(intWritable.get(), 1);
         assertEquals(text.toString(), "hello");
-        
+
         // A bug in ORC InputFormat does not allow empty file in input directory
-        fs.delete(new Path(OUTPUT1, "_SUCCESS"));
-        
+        fs.delete(new Path(OUTPUT1, "_SUCCESS"), true);
+
         // Read the output file back
         pigServer.registerQuery("A = load '" + OUTPUT1 + "' using OrcStorage();");
         Schema s = pigServer.dumpSchema("A");
@@ -190,12 +185,12 @@ public class TestOrcStorage {
         assertEquals(t.size(), 2);
         assertEquals(t.get(0), 65536);
         assertEquals(t.get(1), "world");
-        
+
         t = iter.next();
         assertEquals(t.size(), 2);
         assertEquals(t.get(0), 1);
         assertEquals(t.get(1), "hello");
-        
+
         assertFalse(iter.hasNext());
     }
 
@@ -207,35 +202,168 @@ public class TestOrcStorage {
         pigServer.registerQuery("store B into '" + OUTPUT2 + "' using OrcStorage();");
         pigServer.registerQuery("store B into '" + OUTPUT3 +"' using OrcStorage('-c SNAPPY');");
         pigServer.executeBatch();
-        
+
         Path outputFilePath = new Path(new Path(OUTPUT2), "part-r-00000");
         Reader reader = OrcFile.createReader(fs, outputFilePath);
         assertEquals(reader.getNumberOfRows(), 2);
         assertEquals(reader.getCompression(), CompressionKind.ZLIB);
-        
-        outputFilePath = new Path(new Path(OUTPUT3), "part-r-00000");
-        reader = OrcFile.createReader(fs, outputFilePath);
+
+        Path outputFilePath2 = new Path(new Path(OUTPUT3), "part-r-00000");
+        reader = OrcFile.createReader(fs, outputFilePath2);
         assertEquals(reader.getNumberOfRows(), 2);
         assertEquals(reader.getCompression(), CompressionKind.SNAPPY);
+
+        verifyData(outputFilePath, outputFilePath2, fs, 2);
     }
-    
-    
+
+    @Test
+    public void testMultipleLoadStore() throws Exception {
+        pigServer.registerQuery("A = load '" + basedir + "orc-file-11-format.orc'" + " using OrcStorage();" );
+        pigServer.registerQuery("store A into '" + OUTPUT1 + "' using OrcStorage();");
+        pigServer.registerQuery("B = load '" + OUTPUT1 + "' using OrcStorage();");
+        verifyData(new Path(basedir + "orc-file-11-format.orc"), pigServer.openIterator("B"), fs, 7500);
+    }
+
     @Test
     public void testLoadStoreMoreDataType() throws Exception {
         pigServer.registerQuery("A = load '" + basedir + "orc-file-11-format.orc'" + " using OrcStorage();" );
         pigServer.registerQuery("B = foreach A generate boolean1..double1, '' as bytes1, string1..;");
         pigServer.store("B", OUTPUT4, "OrcStorage");
-        
+
         // A bug in ORC InputFormat does not allow empty file in input directory
-        fs.delete(new Path(OUTPUT4, "_SUCCESS"));
-        
+        fs.delete(new Path(OUTPUT4, "_SUCCESS"), true);
+
         pigServer.registerQuery("A = load '" + OUTPUT4 + "' using OrcStorage();" );
         Schema s = pigServer.dumpSchema("A");
         Iterator<Tuple> iter = pigServer.openIterator("A");
         Tuple t = iter.next();
         assertTrue(t.toString().startsWith("(false,1,1024,65536,9223372036854775807,1.0,-15.0," +
                 ",hi,({(1,bye),(2,sigh)}),{(3,good),(4,bad)},[],"));
-        assertTrue(t.get(12).toString().matches("2000-03-12T00:00:00.000.*"));
+        assertTrue(t.get(12).toString().matches("2000-03-12T15:00:00.000.*"));
         assertTrue(t.toString().endsWith(",12345678.6547456)"));
     }
+
+    private void verifyData(Path orcFile, Iterator<Tuple> iter, FileSystem fs, int expectedTotalRows) throws Exception {
+
+        int expectedRows = 0;
+        int actualRows = 0;
+        Reader orcReader = OrcFile.createReader(fs, orcFile);
+        ObjectInspector oi = orcReader.getObjectInspector();
+        StructObjectInspector soi = (StructObjectInspector) oi;
+
+        RecordReader reader = orcReader.rows(null);
+        Object row = null;
+
+        while (reader.hasNext()) {
+            row = reader.next(row);
+            expectedRows++;
+            List<?> orcRow = soi.getStructFieldsDataAsList(row);
+            if (!iter.hasNext()) {
+                break;
+            }
+            Tuple t = iter.next();
+            assertEquals(orcRow.size(), t.size());
+            actualRows++;
+
+            for (int i = 0; i < orcRow.size(); i++) {
+                Object expected = orcRow.get(i);
+                Object actual = t.get(i);
+                compareData(expected, actual);
+            }
+        }
+        assertFalse(iter.hasNext());
+        assertEquals(expectedRows, actualRows);
+        assertEquals(expectedTotalRows, actualRows);
+
+    }
+
+    private void verifyData(Path orcFile, Path pigOrcFile, FileSystem fs, int expectedTotalRows) throws Exception {
+
+        int expectedRows = 0;
+        int actualRows = 0;
+        Reader orcReaderExpected = OrcFile.createReader(fs, orcFile);
+        StructObjectInspector soiExpected = (StructObjectInspector) orcReaderExpected.getObjectInspector();
+        Reader orcReaderActual = OrcFile.createReader(fs, orcFile);
+        StructObjectInspector soiActual = (StructObjectInspector) orcReaderActual.getObjectInspector();
+
+        RecordReader readerExpected = orcReaderExpected.rows(null);
+        Object expectedRow = null;
+        RecordReader readerActual = orcReaderActual.rows(null);
+        Object actualRow = null;
+
+        while (readerExpected.hasNext()) {
+            expectedRow = readerExpected.next(expectedRow);
+            expectedRows++;
+            List<?> orcRowExpected = soiExpected.getStructFieldsDataAsList(expectedRow);
+            if (!readerActual.hasNext()) {
+                break;
+            }
+            actualRow = readerActual.next(actualRow);
+            actualRows++;
+            List<?> orcRowActual = soiActual.getStructFieldsDataAsList(actualRow);
+            assertEquals(orcRowExpected.size(), orcRowActual.size());
+
+            for (int i = 0; i < orcRowExpected.size(); i++) {
+                assertEquals(orcRowExpected.get(i), orcRowActual.get(i));
+            }
+        }
+        assertFalse(readerActual.hasNext());
+        assertEquals(expectedRows, actualRows);
+        assertEquals(expectedTotalRows, actualRows);
+
+    }
+
+    private void compareData(Object expected, Object actual) {
+        if (expected instanceof Text) {
+            assertEquals(String.class, actual.getClass());
+            assertEquals(expected.toString(), actual);
+        } else if (expected instanceof ShortWritable) {
+            assertEquals(Integer.class, actual.getClass());
+            assertEquals((int)((ShortWritable) expected).get(), actual);
+        } else if (expected instanceof IntWritable) {
+            assertEquals(Integer.class, actual.getClass());
+            assertEquals(((IntWritable) expected).get(), actual);
+        } else if (expected instanceof LongWritable) {
+            assertEquals(Long.class, actual.getClass());
+            assertEquals(((LongWritable) expected).get(), actual);
+        } else if (expected instanceof FloatWritable) {
+            assertEquals(Float.class, actual.getClass());
+            assertEquals(((FloatWritable) expected).get(), actual);
+        } else if (expected instanceof HiveDecimalWritable) {
+            assertEquals(BigDecimal.class, actual.getClass());
+            assertEquals(((HiveDecimalWritable) expected).toString(), actual.toString());
+        } else if (expected instanceof DoubleWritable) {
+            assertEquals(Double.class, actual.getClass());
+            assertEquals(((DoubleWritable) expected).get(), actual);
+        } else if (expected instanceof BooleanWritable) {
+            assertEquals(Boolean.class, actual.getClass());
+            assertEquals(((BooleanWritable) expected).get(), actual);
+        } else if (expected instanceof TimestampWritable) {
+            assertEquals(DateTime.class, actual.getClass());
+            assertEquals(((TimestampWritable) expected).getTimestamp().getTime(),
+                    ((DateTime) actual).getMillis());
+        } else if (expected instanceof BytesWritable) {
+            assertEquals(DataByteArray.class, actual.getClass());
+            BytesWritable bw = (BytesWritable) expected;
+            assertEquals(new DataByteArray(bw.getBytes(), 0, bw.getLength()), actual);
+        } else if (expected instanceof ByteWritable) {
+            assertEquals(Integer.class, actual.getClass());
+            assertEquals((int) ((ByteWritable) expected).get(), actual);
+        } else if (expected instanceof OrcStruct) {
+            assertEquals(BinSedesTuple.class, actual.getClass());
+            // TODO: compare actual values. No getters in OrcStruct
+        } else if (expected instanceof ArrayList) {
+            assertEquals(DefaultDataBag.class, actual.getClass());
+            // TODO: compare actual values. No getters in OrcStruct
+        } else if (expected instanceof HashMap) {
+            assertEquals(HashMap.class, actual.getClass());
+            assertEquals(((HashMap) expected).size(), ((HashMap) actual).size());
+            // TODO: compare actual values. No getters in OrcStruct
+        } else if (expected == null) {
+            assertEquals(expected, actual);
+        } else {
+            Assert.fail("Unknown object type: " + expected.getClass().getName());
+        }
+    }
+
 }
