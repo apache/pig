8cc1fbefaca332e7cf18ae1a2471c140d8ad07f3
Jianyong Dai <daijy@apache.org>
2014-08-22 03:50:45 +0000

    PIG-4136: No special handling jythonjar/jrubyjar in e2e tests after PIG-4047
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1619656 13f79535-47bb-0310-9956-ffa450edef68

--- test/e2e/pig/drivers/Util.pm
+++ test/e2e/pig/drivers/Util.pm
@@ -253,12 +253,6 @@ sub getPigCmd
             # use user provided jar
                 my $classpath;
 
-				if (defined $properties->{'jythonjar'}) {
-					$classpath = "$classpath:" . $properties->{'jythonjar'};
-				}
-				if (defined $properties->{'jrubyjar'}) {
-					$classpath = "$classpath:" . $properties->{'jrubyjar'};
-				}
                 if( $properties->{'exectype'} eq "local") {
                    # in local mode, we should not use
                    # any hadoop-site.xml
