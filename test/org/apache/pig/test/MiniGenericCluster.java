686a04bc27b4b795a5ce61fccc6ef47910b293bf
praveen <praveen@unknown>
2014-10-29 12:12:22 +0000

    PIG-4168: Initial implementation of unit tests (liyunzhang via praveen)
    git-svn-id: https://svn.apache.org/repos/asf/pig/branches/spark@1635107 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/test/MiniGenericCluster.java
+++ test/org/apache/pig/test/MiniGenericCluster.java
@@ -47,6 +47,7 @@ abstract public class MiniGenericCluster {
 
     public static String EXECTYPE_MR = "mr";
     public static String EXECTYPE_TEZ = "tez";
+    public static String EXECTYPE_SPARK = "spark";
 
     /**
      * Returns the single instance of class MiniGenericCluster that represents
@@ -74,6 +75,8 @@ abstract public class MiniGenericCluster {
                 INSTANCE = new MiniCluster();
             } else if (execType.equalsIgnoreCase(EXECTYPE_TEZ)) {
                 INSTANCE = new TezMiniCluster();
+            } else if (execType.equalsIgnoreCase(EXECTYPE_SPARK)) {
+                INSTANCE = new SparkMiniCluster();
             } else {
                 throw new RuntimeException("Unknown test.exec.type: " + execType);
             }
