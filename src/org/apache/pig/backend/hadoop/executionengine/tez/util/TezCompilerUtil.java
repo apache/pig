1b5afa30628d534359e6678d8a9e62e9b49d9564
Jianyong Dai <daijy@apache.org>
2015-01-13 21:27:05 +0000

    PIG-4379: Make RoundRobinPartitioner public
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1651505 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/backend/hadoop/executionengine/tez/util/TezCompilerUtil.java
+++ src/org/apache/pig/backend/hadoop/executionengine/tez/util/TezCompilerUtil.java
@@ -37,7 +37,7 @@ import org.apache.pig.backend.hadoop.executionengine.tez.plan.TezOperator;
 import org.apache.pig.backend.hadoop.executionengine.tez.plan.operator.POLocalRearrangeTez;
 import org.apache.pig.backend.hadoop.executionengine.tez.plan.operator.POStoreTez;
 import org.apache.pig.backend.hadoop.executionengine.tez.plan.operator.POValueOutputTez;
-import org.apache.pig.backend.hadoop.executionengine.tez.runtime.RoundRobinPartitioner;
+import org.apache.pig.builtin.RoundRobinPartitioner;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.TupleFactory;
 import org.apache.pig.impl.PigContext;
