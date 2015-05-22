98063272b48483a08f007762d0d5430f5fc33bd8
Rohini Palaniswamy <rohini@apache.org>
2015-05-22 21:36:50 +0000

    PIG-3994: Implement getting backend exception for Tez (rohini)
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1681240 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/pig/test/TestEvalPipeline2.java
+++ test/org/apache/pig/test/TestEvalPipeline2.java
@@ -60,7 +60,6 @@ import org.apache.pig.impl.util.ObjectSerializer;
 import org.apache.pig.test.utils.Identity;
 import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -1416,7 +1415,6 @@ public class TestEvalPipeline2 {
     // See PIG-1826
     @Test
     public void testNonStandardData() throws Exception{
-        Assume.assumeTrue("Skip this test for TEZ. See PIG-3994", Util.isMapredExecType(cluster.getExecType()));
         String[] input1 = {
                 "0",
         };
@@ -1429,7 +1427,10 @@ public class TestEvalPipeline2 {
             pigServer.openIterator("b");
             Assert.fail();
         } catch (Exception e) {
-            String message = e.getCause().getCause().getMessage();
+            // Tez does not construct exceptions from stacktrace as it will have multiple ones.
+            // So e.getCause().getCause() will be null
+            Throwable cause = e.getCause().getCause() ==  null ? e.getCause() : e.getCause().getCause();
+            String message = cause.getMessage();
             Assert.assertTrue(message.contains(ArrayList.class.getName()));
         }
     }
@@ -1437,7 +1438,6 @@ public class TestEvalPipeline2 {
     // See PIG-1826
     @Test
     public void testNonStandardDataWithoutFetch() throws Exception{
-        Assume.assumeTrue("Skip this test for TEZ. See PIG-3994", Util.isMapredExecType(cluster.getExecType()));
         Properties props = pigServer.getPigContext().getProperties();
         props.setProperty(PigConfiguration.PIG_OPT_FETCH, "false");
         String[] input1 = {
@@ -1674,17 +1674,17 @@ public class TestEvalPipeline2 {
     public void testRankWithEmptyReduce() throws Exception {
         Util.createInputFile(cluster, "table_testRankWithEmptyReduce", new String[]{"1\t2\t3", "4\t5\t6", "7\t8\t9"});
         pigServer.setDefaultParallel(4);
-        
+
         pigServer.registerQuery("d = load 'table_testRankWithEmptyReduce' as (a:int, b:int, c:int);");
         pigServer.registerQuery("e = rank d by a parallel 4;");
-        
+
         Iterator<Tuple> iter = pigServer.openIterator("e");
 
         Collection<String> results = new HashSet<String>();
         results.add("(1,1,2,3)");
         results.add("(2,4,5,6)");
         results.add("(3,7,8,9)");
-        
+
         Assert.assertTrue(results.contains(iter.next().toString()));
         Assert.assertTrue(results.contains(iter.next().toString()));
         Assert.assertTrue(results.contains(iter.next().toString()));
