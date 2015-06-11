e3430507eb5158924d8814ab3315e04b1b96493c
Xuefu Zhang <xuefu@apache.org>
2015-06-11 13:10:59 +0000

    PIG-4593: Enable TestMultiQueryLocal in spark mode (Liyun via Xuefu)
    git-svn-id: https://svn.apache.org/repos/asf/pig/branches/spark@1684877 13f79535-47bb-0310-9956-ffa450edef68

--- shims/test/hadoop23/org/apache/pig/test/SparkMiniCluster.java
+++ shims/test/hadoop23/org/apache/pig/test/SparkMiniCluster.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.pig.test;
 
 import java.io.File;
@@ -14,7 +31,9 @@ import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.mapreduce.v2.MiniMRYarnCluster;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.pig.ExecType;
+import org.apache.pig.backend.hadoop.executionengine.Launcher;
 import org.apache.pig.backend.hadoop.executionengine.spark.SparkExecType;
+import org.apache.pig.backend.hadoop.executionengine.spark.SparkLauncher;
 
 public class SparkMiniCluster extends MiniGenericCluster {
     private static final File CONF_DIR = new File("build/classes");
@@ -124,5 +143,9 @@ public class SparkMiniCluster extends MiniGenericCluster {
         if(YARN_CONF_FILE.exists()) {
             YARN_CONF_FILE.delete();
         }
+    }
+
+    static public Launcher getLauncher() {
+        return new SparkLauncher();
     }
 }
