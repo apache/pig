1db1a1c8207968a69fafd95bb94f9dab0c3bad98
Jianyong Dai <daijy@apache.org>
2015-08-05 17:41:44 +0000

    PIG-4638: Allow TOMAP to accept dynamically sized input
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1694271 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/builtin/TOMAP.java
+++ src/org/apache/pig/builtin/TOMAP.java
@@ -18,10 +18,12 @@
 package org.apache.pig.builtin;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
 
 import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataBag;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
@@ -30,27 +32,53 @@ import org.apache.pig.impl.logicalLayer.schema.Schema;
  * This class makes a map out of the parameters passed to it
  * T = foreach U generate TOMAP($0, $1, $2, $3);
  * It generates a map $0->1, $2->$3
+ *
+ * This UDF also accepts a bag with 'pair' tuples (i.e. tuples with a 'key' and a 'value').
+ *
  */
 public class TOMAP extends EvalFunc<Map> {
 
     @Override
     public Map exec(Tuple input) throws IOException {
-	if (input == null || input.size() < 2)
-		return null;
-        try {
-	    Map<String, Object> output = new HashMap<String, Object>();
+        if (input == null || input.size() == 0) {
+            return null;
+        }
 
-            for (int i = 0; i < input.size(); i=i+2) {
-		String key = (String)input.get(i);
-		Object val = input.get(i+1);	
-		output.put(key, val);
+        Map<String, Object> output = new HashMap<String, Object>();
+
+        try {
+            // Is this a single bag with all the values?
+            if (input.size() == 1) {
+                if (input.get(0) instanceof DataBag) {
+                    DataBag bagOfPairs = (DataBag)input.get(0);
+                    if (bagOfPairs.size() == 0) {
+                        return output;
+                    }
+
+                    for (Tuple tuple: bagOfPairs) {
+                        if (tuple.size() != 2) {
+                            throw new RuntimeException("All input tuples in the bag MUST have exactly 2 fields");
+                        }
+                        String key = (String)tuple.get(0);
+                        Object val = tuple.get(1);
+                        output.put(key, val);
+                    }
+                    return output;
+                } else {
+                    return null; // If only 1 value then it must be a bag
+                }
             }
 
-	    return output;
-	} catch (ClassCastException e){
-		throw new RuntimeException("Map key must be a String");
-	} catch (ArrayIndexOutOfBoundsException e){
-		throw new RuntimeException("Function input must have even number of parameters");
+            for (int i = 0; i < input.size(); i=i+2) {
+                String key = (String)input.get(i);
+                Object val = input.get(i+1);
+                output.put(key, val);
+            }
+            return output;
+        } catch (ClassCastException e){
+            throw new RuntimeException("Map key must be a String");
+        } catch (ArrayIndexOutOfBoundsException e){
+            throw new RuntimeException("Function input must have even number of parameters");
         } catch (Exception e) {
             throw new RuntimeException("Error while creating a map", e);
         }
