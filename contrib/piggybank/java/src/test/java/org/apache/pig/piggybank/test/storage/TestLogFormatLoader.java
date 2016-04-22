484fab7055ad867b8cf5b32d33a7e406fd2ffbf9
Jianyong Dai <daijy@apache.org>
2016-04-22 22:47:13 +0000

    PIG-4877: LogFormat parser fails test
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1740619 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/piggybank/java/src/test/java/org/apache/pig/piggybank/test/storage/TestLogFormatLoader.java
+++ contrib/piggybank/java/src/test/java/org/apache/pig/piggybank/test/storage/TestLogFormatLoader.java
@@ -109,7 +109,7 @@ public class TestLogFormatLoader {
         Tuple actual = out.get(0);
         Tuple expected = tuple(
             "2001:980:91c0:1:8d31:a232:25e5:85d",
-            "[05/Sep/2010:11:27:50 +0200]",
+            "05/Sep/2010:11:27:50 +0200",
             "koken-pannen_303_hs-koken-pannen-afj-120601_B3_product_1_9200000002876066",
             map(
                 "promo"       , "koken-pannen_303_hs-koken-pannen-afj-120601_B3_product_1_9200000002876066",
