69d54d16976914d4e8d6a7bfd83f12b3d6e896dc
Rohini Palaniswamy <rohini@apache.org>
2017-05-17 21:52:19 +0000

    PIG-5135: HDFS bytes read stats are always 0 in Spark mode (szita via rohini)
    git-svn-id: https://svn.apache.org/repos/asf/pig/branches/spark@1795466 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/backend/hadoop/executionengine/spark/running/PigInputFormatSpark.java
+++ src/org/apache/pig/backend/hadoop/executionengine/spark/running/PigInputFormatSpark.java
@@ -18,19 +18,25 @@
 package org.apache.pig.backend.hadoop.executionengine.spark.running;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.JobID;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigHadoopLogger;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigInputFormat;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigMapReduce;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
 import org.apache.pig.backend.hadoop.executionengine.physicalLayer.PhysicalOperator;
 import org.apache.pig.backend.hadoop.executionengine.shims.HadoopShims;
+import org.apache.pig.backend.hadoop.executionengine.spark.SparkPigRecordReader;
+import org.apache.pig.backend.hadoop.executionengine.spark.SparkPigSplit;
 import org.apache.pig.backend.hadoop.executionengine.util.MapRedUtil;
 import org.apache.pig.data.SchemaTupleBackend;
 import org.apache.pig.data.Tuple;
@@ -42,14 +48,13 @@ import org.apache.pig.tools.pigstats.spark.SparkCounters;
 
 public class PigInputFormatSpark extends PigInputFormat {
 
-	@Override
-	public RecordReader<Text, Tuple> createRecordReader(InputSplit split,
-			TaskAttemptContext context) throws IOException,
-			InterruptedException {
+    @Override
+    public RecordReader<Text, Tuple> createRecordReader(InputSplit split, TaskAttemptContext context) throws
+            IOException, InterruptedException {
         resetUDFContext();
         //PigSplit#conf is the default hadoop configuration, we need get the configuration
         //from context.getConfigration() to retrieve pig properties
-        PigSplit pigSplit = (PigSplit) split;
+        PigSplit pigSplit = ((SparkPigSplit) split).getWrappedPigSplit();
         Configuration conf = context.getConfiguration();
         pigSplit.setConf(conf);
         //Set current splitIndex in PigMapReduce.sJobContext.getConfiguration.get(PigImplConstants.PIG_SPLIT_INDEX)
@@ -61,7 +66,42 @@ public class PigInputFormatSpark extends PigInputFormat {
         // Here JobConf is first available in spark Executor thread, we initialize PigContext,UDFContext and
         // SchemaTupleBackend by reading properties from JobConf
         initialize(conf);
-        return super.createRecordReader(split, context);
+
+        SparkRecordReaderFactory sparkRecordReaderFactory = new SparkRecordReaderFactory(pigSplit, context);
+        return sparkRecordReaderFactory.createRecordReader();
+    }
+
+    /**
+     * This is where we have to wrap PigSplits into SparkPigSplits
+     * @param jobcontext
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    public List<InputSplit> getSplits(JobContext jobcontext) throws IOException, InterruptedException {
+        List<InputSplit> sparkPigSplits = new ArrayList<>();
+        List<InputSplit> originalSplits = super.getSplits(jobcontext);
+
+        boolean isFileSplits = true;
+        for (InputSplit inputSplit : originalSplits) {
+            PigSplit split = (PigSplit)inputSplit;
+            if (!(split.getWrappedSplit() instanceof FileSplit)) {
+                isFileSplits = false;
+                break;
+            }
+        }
+
+        for (InputSplit inputSplit : originalSplits) {
+            PigSplit split = (PigSplit) inputSplit;
+            if (!isFileSplits) {
+                sparkPigSplits.add(new SparkPigSplit.GenericSparkPigSplit(split));
+            } else {
+                sparkPigSplits.add(new SparkPigSplit.FileSparkPigSplit(split));
+            }
+        }
+
+        return sparkPigSplits;
     }
 
     private void initialize(Configuration jobConf) throws IOException {
@@ -78,4 +118,17 @@ public class PigInputFormatSpark extends PigInputFormat {
     private void resetUDFContext() {
 		UDFContext.getUDFContext().reset();
 	}
+
+
+    static class SparkRecordReaderFactory extends PigInputFormat.RecordReaderFactory {
+
+        public SparkRecordReaderFactory(InputSplit split, TaskAttemptContext context) throws IOException {
+            super(split, context);
+        }
+
+        @Override
+        public RecordReader<Text, Tuple> createRecordReader() throws IOException, InterruptedException {
+            return new SparkPigRecordReader(inputFormat, pigSplit, loadFunc, context, limit);
+        }
+    }
 }
