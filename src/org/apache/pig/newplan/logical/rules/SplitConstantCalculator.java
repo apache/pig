ca4d681a67d423d05f33beade4efa772b86a70d4
Koji Noguchi <knoguchi@apache.org>
2017-12-28 03:46:58 +0000

    PIG-5322: adding missing SplitConstantCalculator.java from previous commit
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1819390 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/pig/newplan/logical/rules/SplitConstantCalculator.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pig.newplan.logical.rules;
+
+import org.apache.pig.impl.PigContext;
+import org.apache.pig.newplan.OperatorPlan;
+import org.apache.pig.newplan.logical.relational.LOSplitOutput;
+import org.apache.pig.newplan.logical.relational.LogicalPlan;
+import org.apache.pig.newplan.logical.relational.LogicalRelationalOperator;
+
+public class SplitConstantCalculator extends ConstantCalculator {
+
+    public SplitConstantCalculator(String n, PigContext pc) {
+        super(n, pc);
+    }
+
+    @Override
+    protected OperatorPlan buildPattern() {
+        LogicalPlan plan = new LogicalPlan();
+        LogicalRelationalOperator op = new LOSplitOutput(plan);
+        plan.add(op);
+        return plan;
+    }
+}
