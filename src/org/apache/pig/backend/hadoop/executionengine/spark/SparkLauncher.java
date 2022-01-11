a2fb029733201e1bee86fc4836387c0df36587a6
Koji Noguchi <knoguchi@apache.org>
2022-01-11 20:38:51 +0000

    PIG-5398: SparkLauncher does not read SPARK_CONF_DIR/spark-defaults.conf (knoguchi)
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1896927 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/backend/hadoop/executionengine/spark/SparkLauncher.java
+++ src/org/apache/pig/backend/hadoop/executionengine/spark/SparkLauncher.java
@@ -18,9 +18,12 @@
 package org.apache.pig.backend.hadoop.executionengine.spark;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -595,6 +598,30 @@ public class SparkLauncher extends Launcher {
             } else {
                 LOG.warn("SPARK_HOME is not set");
             }
+
+            String sparkConfEnv = System.getenv("SPARK_CONF_DIR");
+            if( sparkConfEnv == null && sparkHome != null) {
+                sparkConfEnv = sparkHome + "/conf";
+            }
+            if( sparkConfEnv != null ) {
+                try {
+                    Properties props = new Properties();
+                    File propsFile = new File (sparkConfEnv,"spark-defaults.conf");
+                    if (propsFile.isFile()) {
+                        try (InputStreamReader isr = new InputStreamReader( 
+                            new FileInputStream(propsFile), StandardCharsets.UTF_8)) {
+                            props.load(isr);
+                            for (Map.Entry<Object, Object> e : props.entrySet()) {
+                                pigCtxtProperties.setProperty(e.getKey().toString(), 
+                                                     e.getValue().toString().trim());
+                            }
+                        }
+                    }
+                } catch (IOException ex) {
+                  LOG.warn("Reading $SPARK_CONF_DIR/spark-defaults.conf failed");
+                }
+            }
+
 
             //Copy all spark.* properties to SparkConf
             for (String key : pigCtxtProperties.stringPropertyNames()) {
